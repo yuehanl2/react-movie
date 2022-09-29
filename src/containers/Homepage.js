65fb114c602b0f76de4d7174058d0a814ba1bd07
MaggieHJZ <114177904+MaggieHJZ@users.noreply.github.com>
2022-09-29 12:52:41 -0700

    add lists

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,11 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Homepage = () => {
+import Carousel from 'react-bootstrap/Carousel';
+import Spinner from 'react-bootstrap/Spinner';
+import '../App.css';
+
+const API_KEY = 'f65a95b51f28aed8c5213b73c573a0e2';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+function Homepage() {
+  const [imgs, setImags] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const timer = setTimeout(() => {
+      fetchHeros();
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  const fetchHeros = async () => {
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setImags(data.results.slice(0, 5));
+      console.log(imgs);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div className="loader">
+        <Spinner animation="border" />
+      </div>
+    );
+  }
+
+  const handleSelect = () => {
+    if (index === imgs.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
   return (
-    <div>
-      <div>Homepage</div>
+    <div className="carousel">
+      <Carousel
+        onSelect={handleSelect}
+        variant="dark"
+        style={{
+          margin: 'auto',
+          width: '85%',
+          height: '80%',
+          marginTop: '2em',
+          opacity: '0.9',
+        }}
+      >
+        <Carousel.Item interval={3000}>
+          {/* <Button>&lt;</Button> */}
+          {imgs.length !== 0 && (
+            <img
+              className="sd-block w-100"
+              src={`${IMG_URL + imgs[index].backdrop_path}`}
+              alt="slide"
+            />
+          )}
+          <Carousel.Caption>
+            {' '}
+            <h5>{imgs.length !== 0 && imgs[index].title}</h5>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
-};
+}
 
 export default Homepage;
--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,79 +1,96 @@
+import React from 'react';
 
 import { getConst } from '../constants';
 import SortButton from '../components/sortButton';
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import '../App.css';
 
 
-export default function ListMove(props){
+export default function MovieList(props) {
+  // props.setLikeListFunction([2])
+  const clickLike = (moive) => {
+    const list = [...props.likeList]
+    const thisMoiveIndex = list.findIndex(item => {
+      return item.id === moive.id
+    })
+    if (thisMoiveIndex === -1) {
+      list.push(moive)
+    } else {
+      list.splice(thisMoiveIndex,1)
+    }    
+    props.setLikeListFunction(list)
+  }
+  const clickBlock = (moive) => {
+    const list = [...props.blockList]
+    const thisMoiveIndex = list.findIndex(item => {
+      return item.id === moive.id
+    })
+    if (thisMoiveIndex === -1) {
+      list.push(moive)
+    } else {
+      list.splice(thisMoiveIndex,1)
+    }    
+    props.setBlockListFunction(list)
+  }
+  return (
+    <>
+      <SortButton />
+      <div className="wrapper">
+        {props.movieList.filter(movie => !props.blockedIdList.has(movie.id)).map((movie) => (
+          <>
+            <div className="item" key={movie.id}>
+              <Card
+                key={movie.id}
+                style={{
+                  height: '65vh',
+                  padding: '4px',
+                  overflow: 'scroll',
+                  marginBottom: '2em',
+                }}
+              >
+                <Card.Img
+                  variant="top"
+                  src={`${getConst.IMG_URL + movie.poster_path}`}
+                  style={{
+                    width: '100%',
+                    height: '45vh',
+                    objectFit: 'cover',
+                    padding: '8px',
+                  }}
+                />
+                <Card.Body>
+                  <Card.Title
+                    style={{ display: 'flex', justifyContent: 'space-between' }}
+                  >
+                    {movie.title}
+                    <span>{movie.vote_average}</span>
+                  </Card.Title>
 
-    
-
-    return(<>
-    <SortButton></SortButton>
-    
-
-
-        {/* {props.movieList.map((item=>
-        <div style={{border:'2px solid black',margin:"10px"}}>
-            <img alt='img' src={getConst.IMG_URL+item.poster_path} height={200} width={150}></img>
-            <div key={item.id}>{item.title}</div>
-            <div>relase Date: {item.release_date}</div>         
-            <div>Vote count: {item.vote_count}</div>
-            <div>Vote Average: {item.vote_average}</div>
-        </div>))} */}
-        
-{props.movieList.map((movie=>
-        <>
-          <div className="item" key={movie.id}>
-  <Card
-    key={movie.id}
-    style={{
-      height: '65vh',
-      padding: '4px',
-      overflow: 'scroll',
-      marginBottom: '2em',
-    }}
-  >
-    <Card.Img
-      variant="top"
-      src={`${getConst.IMG_URL + movie.poster_path}`}
-      style={{
-        width: '100%',
-        height: '45vh',
-        objectFit: 'cover',
-        padding: '8px',
-      }}
-    />
-    <Card.Body>
-      <Card.Title>{movie.title}</Card.Title>
-      <Card.Text
-        style={{ display: 'flex', justifyContent: 'space-between' }}
-      >
-        {' '}
-        {movie.release_date}{' '}
-        <span>
-          <Button variant="outline-danger" size="sm">
-            Like
-          </Button>{' '}
-          <Button variant="outline-primary" size="sm">
-            Block
-          </Button>
-        </span>
-      </Card.Text>
-      <Card.Text style={{ fontSize: '14px' }}>
-        {movie.overview}
-      </Card.Text>
-    </Card.Body>
-  </Card>
-</div>
-        
-        </>))} 
-
-    
-    
-    </>)
+                  <Card.Text
+                    style={{ display: 'flex', justifyContent: 'space-between' }}
+                  >
+                    {' '}
+                    {movie.release_date}{' '}
+                    <span>
+                      <Button variant="outline-danger" size="sm" onClick={() => {clickLike(movie)}}>
+                        {props.likeList.findIndex(item => item.id === movie.id) === -1 ?'Like':'Dislike'}
+                      </Button>{' '}
+                      <Button variant="outline-primary" size="sm" onClick={() => {clickBlock(movie)}}>
+                        {/* block */}
+                        {props.blockList.findIndex(item => item.id === movie.id) === -1?'Block':'Unblock'}
+                      </Button>
+                    </span>
+                  </Card.Text>
+                  <Card.Text style={{ fontSize: '14px' }}>
+                    {movie.overview}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </>
+        ))}
+      </div>
+    </>
+  );
 }
-
